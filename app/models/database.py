d7c0b4c9da1c396128778dbc4f90f45ec9ce039f
wooyeol.park <twinis@gmail.com>
2024-08-12 14:54:07 +0900

    feat: DDL Query 반영 DB 접속 모듈 수정

--- app/models/database.py
+++ app/models/database.py
@@ -2,16 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://{userID}:{password}@{hostname}/{dbname}"
+DATABASE_URL = "sqlite:///app/models/dsdb.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
