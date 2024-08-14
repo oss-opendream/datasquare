c62242c77eea68a5f2013a36e6825be84e1a82aa
yeonhlee <lyhldy@naver.com>
2024-08-14 23:07:57 +0900

    feat issue_publish redirect to views page

--- app/routers/issue_publish.py
+++ app/routers/issue_publish.py
@@ -5,11 +5,11 @@ from datetime import date, datetime, timezone, timedelta
 
 from fastapi import APIRouter, Request, Depends, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
 
 from app.models.database import Base, engine, SessionLocal
-from app.models.issue import Issue
+from app.models.issue import Issue, IssueComment
 
 
 Base.metadata.create_all(bind=engine)
@@ -39,7 +39,7 @@ async def create_issue(request: Request,
     user_id = "1"
     ###
     tz = timezone(timedelta(hours=9))
-    current_time = datetime.now(tz=tz).strftime("%Y-%m-%d-%H:%M:%S")
+    current_time = datetime.now(tz=tz).strftime("%Y-%m-%d%H%M%S")
 
     new_issue = Issue(
         title=title,
@@ -53,10 +53,20 @@ async def create_issue(request: Request,
     db.add(new_issue)
     db.commit()
     db.refresh(new_issue)
-    issue = db.query(Issue).all()
-    return templates.TemplateResponse(
-        "issue_publish.html", {"request": request, "issues": issue}
+
+    # 새로운 IssueComment 객체 생성
+    new_comment = IssueComment(
+        # comment_id=new_issue.issue_id,
+        publisher_id=user_id,       # 이 부분은 숫자 ID로 대체되어야 합니다.
+        within=new_issue.issue_id,  # Issue의 ID를 외래 키로 사용
+        content="Init new issue!!!!"
     )
+
+    db.add(new_comment)
+    db.commit()
+    db.refresh(new_comment)
+
+    return RedirectResponse(url=f"/issue/views/{new_comment.comment_id}", status_code=303)
 
 
 @router.get("/issue/publish", response_class=HTMLResponse)
