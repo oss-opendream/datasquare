6cf4ae5101a1e42a1c9d75db1758ec108e02a25e
wooyeol.park <twinis@gmail.com>
2024-08-16 15:08:43 +0900

    feat: issue 팀명 검색 구현

--- app/routers/feed.py
+++ app/routers/feed.py
@@ -3,17 +3,23 @@ import base64
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from sqlalchemy.dialects import sqlite
 
 from app.models.database import Base, SessionLocal, engine
-from app.models.issue import Issue, TeamProfile, TeamMembership
+from app.models.issue import Issue, PersonalProfile, TeamProfile, TeamMembership
+
 
 Base.metadata.create_all(bind=engine)
 
 router = APIRouter()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory='templates')
 
 
 def get_db():
+    '''
+    DB 세션 관리 함수
+    '''
+
     db = SessionLocal()
     try:
         yield db
@@ -21,77 +27,96 @@ def get_db():
         db.close()
 
 
-@router.get("/feed")
+@router.get('/feed')
 async def read_dashboard(request: Request, db: Session = Depends(get_db)):
+    '''
+    조직 내 공개된 전체 이슈 목록 출력 함수
+    '''
+
     # retrieve teams list
     teams = db.query(TeamProfile).with_entities(
         TeamProfile.team_name, TeamProfile.profile_id).all()
 
-    issues = db.query(Issue).filter_by(is_private=0).all()
+    issue_base_query = db.query(
+        Issue, PersonalProfile, TeamProfile).filter(Issue.is_private == 0)
+    issues = issue_base_query.join(PersonalProfile, Issue.publisher_id == PersonalProfile.profile_id) \
+        .join(TeamMembership, PersonalProfile.profile_id == TeamMembership.member_id) \
+        .join(TeamProfile, TeamMembership.team_id == TeamProfile.profile_id).all()
 
     issue_data = []
 
-    for issue in issues:
-        author = issue.publisher
-        author_team_name = db.query(TeamMembership).filter_by(
-            member_id=author.profile_id).first().team.team_name
+    for issue, personal_profile, team_profile in issues:
 
         issue_data.append({
-            "title": issue.title,
-            "content": issue.content,
-            "author_name": author.name,
-            "team": author_team_name,
-            "profile_pic": base64.b64encode(author.profile_image).decode("utf-8")
+            'title': issue.title,
+            'content': issue.content,
+            'author_name': personal_profile.name,
+            'team': team_profile.team_name,
+            'profile_pic': base64.b64encode(personal_profile.profile_image).decode('utf-8')
         })
 
-    return templates.TemplateResponse("feed.html", {"request": request, "teams": teams, "issues": issue_data, "teams": teams})
+    return templates.TemplateResponse('feed.html', {'request': request, 'teams': teams, 'issues': issue_data})
 
 
-@router.get("/feed/my_issues")
+@router.get('/feed/my_issues')
 async def read_my_issues(request: Request, db: Session = Depends(get_db)):
+    '''
+    현재 접속자가 작성한 이슈 목록 출력 함수
+    '''
+
     teams = db.query(TeamProfile).with_entities(
         TeamProfile.team_name, TeamProfile.profile_id).all()
     # 'author_id='에 로그인 유저 ID 값 입력 필요(예시 ID: 1)
-    my_issues = db.query(Issue).filter_by(publisher_id=1).all()
+    issue_base_query = db.query(
+        Issue, PersonalProfile, TeamProfile).filter(Issue.publisher_id == 1)
+    issues = issue_base_query.join(PersonalProfile, Issue.publisher_id == PersonalProfile.profile_id) \
+        .join(TeamMembership, PersonalProfile.profile_id == TeamMembership.member_id) \
+        .join(TeamProfile, TeamMembership.team_id == TeamProfile.profile_id).all()
 
     issue_data = []
 
-    for issue in my_issues:
-        author = issue.publisher
-        author_team_name = db.query(TeamMembership).filter_by(
-            member_id=author.profile_id).first().team.team_name
+    for issue, personal_profile, team_profile in issues:
 
         issue_data.append({
-            "title": issue.title,
-            "content": issue.content,
-            "author_name": author.name,
-            "team": author_team_name,
-            "profile_pic": base64.b64encode(author.profile_image).decode("utf-8")
+            'title': issue.title,
+            'content': issue.content,
+            'author_name': personal_profile.name,
+            'team': team_profile.team_name,
+            'profile_pic': base64.b64encode(personal_profile.profile_image).decode('utf-8')
         })
 
-    return templates.TemplateResponse("feed.html", {"request": request, "teams": teams, "issues": issue_data})
+    return templates.TemplateResponse('feed.html', {'request': request, 'teams': teams, 'issues': issue_data})
 
 
-@router.get("/feed/search")
-async def search_issues(request: Request, keyword: str, db: Session = Depends(get_db)):
+@router.get('/feed/search')
+async def search_issues(request: Request, keyword: str, team=str, db: Session = Depends(get_db)):
+    '''
+    제목으로 검색된 이슈 목록 출력 함수
+    '''
+
     teams = db.query(TeamProfile).with_entities(
         TeamProfile.team_name, TeamProfile.profile_id).all()
-    search_result = db.query(Issue).filter(
-        Issue.title.contains(keyword)).all()
+
+    issue_base_query = db.query(Issue, PersonalProfile, TeamProfile) \
+        .filter(Issue.is_private == 0)
+
+    search_result = issue_base_query.join(PersonalProfile, Issue.publisher_id == PersonalProfile.profile_id) \
+        .join(TeamMembership, PersonalProfile.profile_id == TeamMembership.member_id) \
+        .join(TeamProfile, TeamMembership.team_id == TeamProfile.profile_id) \
+        .filter(TeamProfile.team_name.contains(team)) \
+        .filter(Issue.title.contains(keyword)) \
+        .all()
 
     result_data = []
 
-    for issue in search_result:
-        author = issue.publisher
-        author_team_name = db.query(TeamMembership).filter_by(
-            member_id=author.profile_id).first().team.team_name
+    for issue, personal_profile, team_profile in search_result:
 
         result_data.append({
-            "title": issue.title,
-            "content": issue.content,
-            "author_name": author.name,
-            "team": author_team_name,
-            "profile_pic": base64.b64encode(author.profile_image).decode("utf-8")
+            'title': issue.title,
+            'content': issue.content,
+            'author_name': personal_profile.name,
+            'team': team_profile.team_name,
+            'profile_pic': base64.b64encode(personal_profile.profile_image).decode('utf-8')
         })
 
-    return templates.TemplateResponse("feed.html", {"request": request, "teams": teams, "issues": result_data})
+    return templates.TemplateResponse('feed.html', {'request': request, 'teams': teams, 'issues': result_data})
