9d6a5231d3905f756fd259190b6b2836a11099eb
Wooyeol Park <twinis@gmail.com>
2024-08-17 17:27:20 +0900

    feat: main 모듈 복구

--- app/main.py
+++ app/main.py
@@ -1,38 +1,12 @@
-import uvicorn
+'''Initializes the FastAPI Application.'''
+
+
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-from app.routers import feed
 
 
-def create_app() -> None:
-    """
-    FastAPI app 객체를 생성합니다.
-    - 'static' 디렉터리의 파일들을 '/static'으로 라우팅
-    - 모든 라우터를 app에 포함
-    :return: FastAPI app 객체
-    """
-    created_app = FastAPI(title="Datasquare",
-                          description="데이터 협업을 위한 조직 간 커뮤니케이션 플랫폼")
-
-    created_app.mount(
-        "/static", StaticFiles(directory="static"), name="static")
-
-    created_app.include_router(feed.router)
-
-    return created_app
-
-
-app = create_app()
+app = FastAPI()
 
 
 @app.get('/')
 def read_root():
-    """
-    루트 라우터 함수
-    """
     return {'hello': 'world'}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80, reload=True)
