4ee211a04a0cbf98d252e3c4980409074651b848
Wooyeol Park <twinis@gmail.com>
2024-08-18 17:10:12 +0900

    feat: feed base query statement 수정 - inner join -> left join

--- app/crud/feed.py
+++ app/crud/feed.py
@@ -3,6 +3,7 @@
 '''
 import base64
 
+from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
 from app.models.database import datasquare_db
@@ -26,12 +27,10 @@ class IssueData:
         with next(self.db.get_db()) as db_session:
             base_query = db_session \
                 .query(Issue, PersonalProfile, TeamProfile) \
-                .filter(Issue.is_private == 0) \
-                .union(
-                    db_session
-                    .query(Issue, PersonalProfile, TeamProfile)
-                    .filter(Issue.publisher_id == self.current_userid)
-                )
+                .outerjoin(PersonalProfile, Issue.publisher_id == PersonalProfile.profile_id) \
+                .outerjoin(TeamMembership, PersonalProfile.profile_id == TeamMembership.member_id) \
+                .outerjoin(TeamProfile, TeamMembership.team_id == TeamProfile.profile_id) \
+                .filter(or_(Issue.is_private == 0, Issue.publisher_id == self.current_userid))
 
             return base_query
 
@@ -62,11 +61,7 @@ class IssueData:
 
         base_query = self.__create_base_query()
 
-        issues = base_query \
-            .join(PersonalProfile, Issue.publisher_id == PersonalProfile.profile_id) \
-            .join(TeamMembership, PersonalProfile.profile_id == TeamMembership.member_id) \
-            .join(TeamProfile, TeamMembership.team_id == TeamProfile.profile_id) \
-            .all()
+        issues = base_query.all()
 
         result_data = self.__format_issue_data(issues)
 
@@ -80,9 +75,6 @@ class IssueData:
         base_query = self.__create_base_query()
 
         issues = base_query \
-            .join(PersonalProfile, Issue.publisher_id == PersonalProfile.profile_id) \
-            .join(TeamMembership, PersonalProfile.profile_id == TeamMembership.member_id) \
-            .join(TeamProfile, TeamMembership.team_id == TeamProfile.profile_id) \
             .filter(Issue.publisher_id == self.current_userid) \
             .all()
 
@@ -90,7 +82,7 @@ class IssueData:
 
         return result_data
 
-    def search(self, keyword: str = "", team: str = ""):
+    def search(self, keyword: str, team: str):
         '''
         제목 또는 팀명으로 검색된 이슈 목록 출력 함수
         '''
@@ -98,9 +90,6 @@ class IssueData:
         base_query = self.__create_base_query()
 
         search_result = base_query \
-            .join(PersonalProfile, Issue.publisher_id == PersonalProfile.profile_id) \
-            .join(TeamMembership, PersonalProfile.profile_id == TeamMembership.member_id) \
-            .join(TeamProfile, TeamMembership.team_id == TeamProfile.profile_id) \
             .filter(TeamProfile.team_name.contains(team)) \
             .filter(Issue.title.contains(keyword)) \
             .all()
