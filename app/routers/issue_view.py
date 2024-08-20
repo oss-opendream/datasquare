d135fbdf199567e939910e277397de722fae4a09
yeonhlee <lyhldy@naver.com>
2024-08-20 15:42:35 +0900

    feat: 댓글 기능 구현

--- app/routers/issue_view.py
+++ app/routers/issue_view.py
@@ -1,42 +1,68 @@
 '''
-issue_view.py
 issue_id를 기준으로 issue데이터와 issue_comment를 불러옵니다.
 댓글을 등록하면 issue_comment를 생성하고 추가로 출력합니다.
 '''
-from fastapi import APIRouter, Request, Depends, HTTPException
+
+
+from fastapi import APIRouter, Request, HTTPException, Form, Depends
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from sqlalchemy.orm import Session
-
-from app.models.database import Base, engine, get_db
-from app.models.issue import Issue, IssueComment
-from app.utils.time import current_time
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 
-Base.metadata.create_all(bind=engine)
+from app.crud.issue_crud import IssueData
+from app.crud.issue_comment_crud import IssueCommentData
+from app.schemas.user_schema import User
+from app.routers.sign import get_current_user
+
 
 router = APIRouter()
 templates = Jinja2Templates(directory='app/templates')
 
 
-@router.get('/issue/view/issue_id={issue_id}', response_class=HTMLResponse)
+@router.post('/issue/view/create_comment')
+async def create_issue_comment(issue_id: int = Form(...),
+                               comment: str = Form(...),
+                               current_user: User = Depends(get_current_user)):
+    '''issue_commnet 생성 함수 입니다.'''
+
+    IssueCommentData(current_user.profile_id).create_issue_comment(
+        issue_id, comment)
+
+    ret = RedirectResponse(
+        url=f'/issue/view?issue_id={issue_id}', status_code=303)
+
+    return ret
+
+
+@router.get('/issue/view', response_class=HTMLResponse)
 async def issue_views(request: Request,
                       issue_id: int,
-                      db: Session = Depends(get_db)):
+                      current_user: User = Depends(get_current_user)
+                      ):
     '''
     이슈 조회 함수입니다.
     issue_id를 기준으로 issue 데이터와
     issue_comment.within == issue.issue_id인 issue_comment데이터를 불러옵니다.
     '''
-    issue = db.query(Issue).filter(
-        Issue.issue_id == issue_id).one_or_none()
+
+    issue = IssueData(current_user.profile_id).read_issue(issue_id)
+
     if not issue:
         raise HTTPException(status_code=404, detail='Issue not found')
 
-    comments = db.query(IssueComment).filter(
-        IssueComment.within == issue_id).all()
+    comments = IssueCommentData(
+        current_user.profile_id).read_issue_comments(issue_id)
+
     if not comments:
         raise HTTPException(status_code=404, detail='Issue_Comments not found')
 
-    return templates.TemplateResponse('issue_view.html',
-                                      {'request': request, 'issue': issue, 'comments': comments})
+    ret = templates.TemplateResponse(
+        'issue_view.html',
+        {
+            'request': request,
+            'issue': issue,
+            'comments': comments,
+        }
+    )
+
+    return ret
