36affcdda0015c1a1e6c27772a8d640ed74a97b0
Choi Jiyoung <ji0516y@gmail.com>
2024-08-20 16:31:32 +0900

    feat: perform functions for llm issue generation
    docsim.py
    - receive pretrained Word2Vec model for sentence to sentence comparison
    
    llm.py
    - cotain functions that aids the performance of llm for issue generation
    - contain unused function for future development (user_selection())

--- app/utils/docsim.py
+++ app/utils/docsim.py
@@ -2,19 +2,27 @@ import numpy as np
 
 
 class DocSim:
+    '''
+    Calculate similarity between two sentences.
+
+    Receive Word2Vec pretrained model'''
+
     def __init__(self, w2v_model, stopwords=None):
         self.w2v_model = w2v_model
         self.stopwords = stopwords if stopwords is not None else []
 
     def vectorize(self, doc: str) -> np.ndarray:
         """
-        Identify the vector values for each word in the given document
+        Identify the vector values for each word in the given document.
+
         :param doc:
         :return:
         """
+
         doc = doc.lower()
         words = [w for w in doc.split(" ") if w not in self.stopwords]
         word_vecs = []
+
         for word in words:
             try:
                 vec = self.w2v_model[word]
@@ -26,18 +34,28 @@ class DocSim:
         # Assuming that document vector is the mean of all the word vectors
         # PS: There are other & better ways to do it.
         vector = np.mean(word_vecs, axis=0)
+
         return vector
 
     def _cosine_sim(self, vecA, vecB):
-        """Find the cosine similarity distance between two vectors."""
-        csim = np.dot(vecA, vecB) / (np.linalg.norm(vecA) * np.linalg.norm(vecB))
+        """
+        Find the cosine similarity distance between two vectors.
+        """
+
+        csim = np.dot(vecA, vecB) / \
+            (np.linalg.norm(vecA) * np.linalg.norm(vecB))
+
         if np.isnan(np.sum(csim)):
             return 0
+
         return csim
 
     def calculate_similarity(self, source_doc, target_docs=None, threshold=0):
-        """Calculates & returns similarity scores between given source document & all
-        the target documents."""
+        """
+        Calculates & returns similarity scores between given source document & all
+        the target documents.
+        """
+
         if not target_docs:
             return []
 
@@ -46,6 +64,7 @@ class DocSim:
 
         source_vec = self.vectorize(source_doc)
         results = []
+
         for doc in target_docs:
             target_vec = self.vectorize(doc)
             sim_score = self._cosine_sim(source_vec, target_vec)
