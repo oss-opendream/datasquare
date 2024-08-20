58171cc85fca80dfae703cfa36e3c37aedd9efcf
yeonhlee <lyhldy@naver.com>
2024-08-20 15:14:44 +0900

    feat: issue_crud issue_comment_crud issue_publish/cancle_button requested_team 구현

--- app/routers/issue_publish.py
+++ app/routers/issue_publish.py
@@ -1,18 +1,16 @@
-'''
-issue_publish.py
-Issue 생성하는 창을 띄우고 Issue, IssueComment를 생성하는 파일입니다.
-'''
-from fastapi import APIRouter, Request, Depends, Form
+'''Issue 생성하는 창을 띄우고 Issue, IssueComment를 생성하는 파일입니다.'''
+
+
+from fastapi import APIRouter, Request, Form, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
-from sqlalchemy.orm import Session
 
-from app.models.database import Base, engine, get_db
-from app.models.issue import Issue, IssueComment
-from app.utils.time import current_time
+from app.crud.issue_crud import IssueData
+from app.crud.issue_comment_crud import IssueCommentData
+from app.crud.team_crud import TeamData
+from app.schemas.user_schema import User
+from app.routers.sign import get_current_user
 
-
-Base.metadata.create_all(bind=engine)
 
 router = APIRouter()
 templates = Jinja2Templates(directory='app/templates')
@@ -23,48 +21,49 @@ async def create_issue(title: str = Form(...),
                        content: str = Form(...),
                        requested_team: str = Form(...),
                        is_private: int = Form(...),
-                       db: Session = Depends(get_db)
+                       current_user: User = Depends(get_current_user)
                        ):
     '''
     Issue, IssueComment 객체를 생성하고 데이터베이스에 저장,
     issue/view/issue_id={Issue.issue_id} 페이지로 Redirect합니다.
     '''
-    ###
-    # test user_id
-    user_id = '1'
-    ###
-    now = current_time()
-    new_issue = Issue(
+
+    issue_data = IssueData(current_user.profile_id)
+
+    new_issue = issue_data.create_issue(
         title=title,
         content=content,
-        publisher_id=user_id,
         requested_team=requested_team,
-        is_private=is_private,
-        created_at=now,
-        modified_at=now
-    )
-    db.add(new_issue)
-    db.commit()
-    db.refresh(new_issue)
-
-    # 새로운 IssueComment 객체 생성
-    new_comment = IssueComment(
-        publisher_id=user_id,
-        within=new_issue.issue_id,
-        content='Init new issue!!!!'
+        is_private=is_private
     )
 
-    db.add(new_comment)
-    db.commit()
-    db.refresh(new_comment)
+    IssueCommentData(current_user.profile_id).create_issue_comment(
+        new_issue.issue_id, 'Init new issue!!!!')
 
-    return RedirectResponse(url=f'/issue/view/issue_id={new_issue.issue_id}', status_code=303)
+    ret = RedirectResponse(
+        url=f'/issue/view?issue_id={new_issue.issue_id}',
+        status_code=303
+    )
+
+    return ret
 
 
 @router.get('/issue/publish', response_class=HTMLResponse)
-async def issue_pulish(request: Request, db: Session = Depends(get_db)):
+async def issue_pulish(request: Request,
+                       current_user: User = Depends(get_current_user)
+                       ):
     '''
     이슈 발행 페이지 함수입니다.
+    team 종류를 받아와서 html로 던져줍니다.
     '''
-    issues = db.query(Issue).all()
-    return templates.TemplateResponse('issue_publish.html', {'request': request, 'issues': issues})
+
+    departments = TeamData().get_team_name()
+    ret = templates.TemplateResponse(
+        'issue_publish.html',
+        {
+            'request': request,
+            'departments': departments
+        }
+    )
+
+    return ret
