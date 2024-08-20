b1b08319cb7172bfd1e0108ae283f95e08465ce0
Choi Jiyoung <ji0516y@gmail.com>
2024-08-20 16:01:17 +0900

    feat: collect & run llm model for issue generation
    - formed to a single function
    - llama2 7b chat model is used with 4bit quantization (GPU needed)
    - divided into 2 phase using langchain

--- app/services/llm.py
+++ app/services/llm.py
@@ -1,65 +1,39 @@
-from langchain import PromptTemplate
-from llm_utils import (load_model, keyword_extraction, metadata_search, metadata_processing, get_user_query, extract_answer)
+from langchain_core.prompts import PromptTemplate
 
-# Prompts
-phase1_prompt = """From the given user query, extract 10 keywords that represent the data that user needs.
+from app.utils.llm import *
+from app.utils import prompts
 
-### User Query: {user_query}
 
-### Response:
-"""
-
-phase2_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
-
-### Instruction:
-Fill out the 'Request Template' based on the following input with user request and relevant datasets. The 'Request Template' is sent from the user to the relevant datasets ower.
-
-### Input:
-User Request: {user_query}
-
-Relevant Dataset: {dataset_1}
-
-'Request Template'
-To: (format: Dataset owner)
-From: (format: User name, Team name)
-Subject:
-Request detail:
-Data usage purpose:
-Data file format (eg. .csv, .jpeg, .py, .json, etc.):
-Data period: (format: YYYY-MM-DD)
-Data example:
-
-### Response:
-"""
-# 임시 metadata
-data_= {'title':['Resource usage statistics for cloud infrastructure','Server load data during peak and off-peak hours over the last year','Customer complaint records Q1','Customer service logs Q1','Employee performance reviews 2023','Customer feedback from product launch surveys'], 'owner':['Computing Team','Resource Team','Customer Support Team','Customer Support Team','HR','Marketing Team'], 'columns':[[1,2,3,4,5],[2,4,6,1,9],['id','date','purchase','complaint','product'],['id','product','price','date','detail'],['employee id','salary','performance','grading','date'],['id','detail','date','feedback','customer_since','annual purchase']]}
-
-# langchain 구현
-def llm_trigger(data:dict):
+def llm_trigger(data: dict, query: str) -> dict:
+    '''
+    Triggers langchain.
+    '''
 
     llm = load_model()
     metadata = metadata_processing(data)
 
-    # chain 1 (유저 쿼리 받고 키워드 추출단계)
+    # phase 1: extract keywords from the user query & search the relevant metadata
+    phase1_prompt = prompts.phase1
+    phase2_prompt = prompts.phase2
+
     prompt1 = PromptTemplate.from_template(phase1_prompt)
     prompt2 = PromptTemplate.from_template(phase2_prompt)
-    query = "I’m Emily, a customer service operations manager. I am managing a project aimed at reducing customer complaints by 15% over the next quarter. To support this initiative, I need access to customer service data for Q1 to analyze complaint trends and identify key areas for service improvement."
 
-    phase1 = prompt1 | llm | keyword_extraction | metadata_search
+    phase1 = prompt1 | llm
     output_phase1 = phase1.invoke({"user_query": query})
 
-    # 1 번째 데이터만 추출
-    final_data_title = output_phase1[0]
-    final_data_owner = metadata.loc[metadata['title'] == final_data_title, 'owner'].values[0]
+    keywords = keyword_extraction(output_phase1)
+    most_relevant = metadata_search(keywords, metadata)
 
-    # issue 생성
+    # phase 2: select the most relevant metadata and generate issue
+    final_data_title = most_relevant[0]
+    final_data_owner = metadata.loc[metadata['title']
+                                    == final_data_title, 'owner'].values[0]
+
     phase2 = prompt2 | llm
-    output_phase2 = phase2.invoke({'user_query':query, 'dataset_1':f"{final_data_title} owned by {final_data_owner}"})
+    output_phase2 = phase2.invoke(
+        {'user_query': query, 'dataset_1': f"{final_data_title} owned by {final_data_owner}"})
 
-    # 생성된 issue 답변 추출, dictionary 형태로 반환 (각 인자별 추출필요)
     result_dict = extract_answer(output_phase2)
-    
-    return result_dict
 
-if __name__ =="__main__":
-    llm_trigger(data_)
+    return result_dict
