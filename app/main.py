7dd6177a27787be106938b3f0d98e4f24b19c578
Yi Hong-ju <to_hongju@pm.me>
2024-08-26 11:45:11 +0900

    Add SQLite initializtion logic to 'database.py'
    Also import the module(database.py) from 'main.py'.

--- app/main.py
+++ app/main.py
@@ -3,8 +3,14 @@
 
 from fastapi import FastAPI
 
+from app.routers import org
+from app.database import Base, engine
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(org.router)
 
 
 @app.get('/')
