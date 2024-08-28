579a0b46e4980a307daf37b15a85d1b5a34ae3ea
yoonjaeo <140726268+yoonjaeo@users.noreply.github.com>
2024-08-28 22:36:35 +0900

    이슈 댓글 수정

--- app/static/js/issue_comment.js
+++ app/static/js/issue_comment.js
@@ -1,20 +1,26 @@
-let editingCommentId = null;  // 현재 수정 중인 댓글 ID
-let originalText = '';
+let editingCommentId = null; // 현재 수정 중인 댓글 ID
+let originalText = "";
 
 function editComment(commentId) {
-    if (editingCommentId !== null) {
-        alert("You can only edit one comment at a time.");
-        return;
-    }
+  if (editingCommentId !== null) {
+    alert("You can only edit one comment at a time.");
+    return;
+  }
 
-    const commentElement = document.getElementById('comment-' + commentId);
-    const currentText = commentElement.querySelector('.comment-text').innerText;
-    originalText = currentText;
-    const commentPublisher = commentElement.querySelector('.comment-publisher').innerText;
+  const commentElement = document.getElementById("comment-" + commentId);
+  const currentText = commentElement.querySelector(".comment-text").innerText;
+  const image = commentElement.querySelector("#image");
+  originalText = currentText;
+  const commentPublisher =
+    commentElement.querySelector("#comment-publisher").innerText;
 
-    editingCommentId = commentId;  // 현재 수정 중인 댓글 ID 설정
+  editingCommentId = commentId; // 현재 수정 중인 댓글 ID 설정
 
-    commentElement.innerHTML = `
+  commentElement.innerHTML = `
+        <img src="${image.src}" width="30" alt="${commentPublisher}"
+          class="comment-publisher-profile-pic"
+          id="image"
+        />
         <span class="comment-publisher">${commentPublisher}</span>
         <br>
         <input type="text" class="comment-text" value="${currentText}" />
@@ -27,46 +33,51 @@ function editComment(commentId) {
 }
 
 function deleteComment(commentId) {
-    fetch('/issue_comment/delete', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: `comment_id=${commentId}`
+  fetch("/issue_comment/delete", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `comment_id=${commentId}`,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "success") {
+        const commentElement = document.getElementById("comment-" + commentId);
+        commentElement.remove(); // 삭제 성공 후 댓글 요소를 제거
+      } else {
+        alert("Failed to delete comment.");
+      }
     })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === 'success') {
-                const commentElement = document.getElementById('comment-' + commentId);
-                commentElement.remove();  // 삭제 성공 후 댓글 요소를 제거
-            } else {
-                alert('Failed to delete comment.');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Failed to delete comment due to an error.');
-        });
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Failed to delete comment due to an error.");
+    });
 }
 
 function saveComment(commentId) {
+  const commentElement = document.getElementById("comment-" + commentId);
+  const currentText = commentElement.querySelector(".comment-text").value;
+  const image = commentElement.querySelector("#image");
+  const commentPublisher =
+    commentElement.querySelector(".comment-publisher").innerText;
 
-    const commentElement = document.getElementById('comment-' + commentId);
-    const currentText = commentElement.querySelector('.comment-text').value;
-    const commentPublisher = commentElement.querySelector('.comment-publisher').innerText;
-
-    fetch('/issue_comment/modify', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: `comment_id=${commentId}&content=${encodeURIComponent(currentText)}`
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === 'success') {
-                const commentElement = document.getElementById('comment-' + commentId);
-                commentElement.innerHTML = `
+  fetch("/issue_comment/modify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `comment_id=${commentId}&content=${encodeURIComponent(currentText)}`,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "success") {
+        const commentElement = document.getElementById("comment-" + commentId);
+        commentElement.innerHTML = `
+        <img src="${image.src}" width="30" alt="${commentPublisher}"
+          class="comment-publisher-profile-pic"
+          id="image"
+        />
                 <span class="comment-publisher">${commentPublisher}</span>
                 <br>
                 <span class="comment-text">${currentText}</span>
@@ -75,22 +86,28 @@ function saveComment(commentId) {
                 <button class="btn btn--primary issue-comment-button"
                     onclick="deleteComment(${commentId})">Delete</button>
             `;
-                editingCommentId = null;  // 수정 완료 후 초기화
-            } else {
-                alert('Failed to save the comment.');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Failed to save comment due to an error.');
-        });
+        editingCommentId = null; // 수정 완료 후 초기화
+      } else {
+        alert("Failed to save the comment.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Failed to save comment due to an error.");
+    });
 }
 
 function cancelComment(commentId) {
-    const commentElement = document.getElementById('comment-' + commentId);
-    const commentPublisher = commentElement.querySelector('.comment-publisher').innerText;
+  const commentElement = document.getElementById("comment-" + commentId);
+  const image = commentElement.querySelector("#image");
+  const commentPublisher =
+    commentElement.querySelector(".comment-publisher").innerText;
 
-    commentElement.innerHTML = `
+  commentElement.innerHTML = `
+        <img src="${image.src}" width="30" alt="${commentPublisher}"
+          class="comment-publisher-profile-pic"
+          id="image"
+        />
         <span class="comment-publisher">${commentPublisher}</span>
         <br>
         <span class="comment-text">${originalText}</span>
@@ -99,5 +116,5 @@ function cancelComment(commentId) {
         <button class="btn btn--primary issue-comment-button"
             onclick="deleteComment(${commentId})">Delete</button>
     `;
-    editingCommentId = null;  // 수정 취소 후 초기화
+  editingCommentId = null; // 수정 취소 후 초기화
 }
