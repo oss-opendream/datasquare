90cd6c377ebb92249e2d7a195de1c6c0c4f1a1cb
yoonjaeo <2000yoonjae@gmail.com>
2024-10-02 07:44:42 +0000

    exec

--- run_datasquare.sh
+++ run_datasquare.sh
@@ -3,7 +3,8 @@
 # 실행이 실패하면 다음 명령어로 가지 않고 종료되도록 설정
 set -e
 
-/root/.pyenv/bin/pyenv activate ds_env
+exec $SHELL
+/root/.pyenv/bin/pyenv activate ds_env 
 poetry install 
 python3 ./app/utils/key_scribe.py "$@"
 poetry run fastapi run 
