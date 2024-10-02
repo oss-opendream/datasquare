d81aaaaf7be99a8c2ba06e4e1b42f73ed4475983
yoonjaeo <2000yoonjae@gmail.com>
2024-10-02 08:33:54 +0000

    modify poeytry

--- run_datasquare.sh
+++ run_datasquare.sh
@@ -3,6 +3,6 @@
 # 실행이 실패하면 다음 명령어로 가지 않고 종료되도록 설정
 set -e
 
-poetry install 
+/root/.local/share/pypoetry/venv/bin/poetry install 
 python3 ./app/utils/key_scribe.py "$@"
-poetry run fastapi run 
+/root/.local/share/pypoetry/venv/bin/poetry run fastapi run 
