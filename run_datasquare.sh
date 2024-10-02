a6a902071f8a01617332f626b0c749d4ad4f9135
yoonjaeo <2000yoonjae@gmail.com>
2024-10-02 08:23:59 +0000

    pyenv delete

--- run_datasquare.sh
+++ run_datasquare.sh
@@ -3,10 +3,6 @@
 # 실행이 실패하면 다음 명령어로 가지 않고 종료되도록 설정
 set -e
 
-cd /datasquare
-echo asdf
-which pyenv
-# /root/.pyenv/bin/pyenv activate ds_env 
-# poetry install 
-# python3 ./app/utils/key_scribe.py "$@"
-# poetry run fastapi run 
+poetry install 
+python3 ./app/utils/key_scribe.py "$@"
+poetry run fastapi run 
